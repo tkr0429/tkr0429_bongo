511e70c2ef8395a18fd1bcab636042cff636086f
Tokushige <t.tokushige@gmail.com>
2022-02-04 19:08:18 +0900

    Changes to be committed: 	new file:   config.h 	new file:   glcdfont.c 	new file:   keymap.c 	new file:   keymap.c.org 	new file:   rules.mk

---
+++ rules.mk
@@ -0,0 +1,14 @@
+# Bootloader selection
+#BOOTLOADER = dfu
+
+MOUSEKEY_ENABLE     = no
+RGBLIGHT_ENABLE     = yes    # Enable WS2812 RGB underlight.
+OLED_ENABLE         = yes
+OLED_DRIVER         = SSD1306
+LTO_ENABLE          = yes
+WPM_ENABLE          = yes
+# If you want to change the display of OLED, you need to change here
+#SPACE_CADET_ENABLE  = no
+#CONSOLE_ENABLE     = no
+#EXTRAKEY_ENABLE    = no
+#MAGIC_ENABLE       = no
